5a98996defccf6ec1f7a321fea56323d6dcdfb97
venkadesanmurugesan <venkadesanmurugesan2209@gmail.com>
2023-04-19 19:36:38 +0530

    pdf changed

--- main.js
+++ main.js
@@ -19,16 +19,37 @@ const showUploadedImages = (e) => {
 };
 
 function downloadPdf() {
+  // printJS(document.getElementById("cardAsPdf"), "html", {
+  //   printable: null,
+  // });
+
+  // const downloadPage = document.getElementById("cardAsPdf");
+  // let win = window.open("", "", "height=700,width=700"); // Open the window. Its a popup window.
+  // win.document.write(downloadPage.outerHTML); // Write contents in the new window.
+  // win.document.close();
+  // win.print(); // Finally, print the contents.
+
+  // doc.fromHTML(
+  //   `<html><head><title>PDF</title></head><body>` +
+  //     document.getElementById("cardAsPdf").innerHTML +
+  //     `</body></html>`
+  // );
+  // doc.save("file.pdf");
   // const fileName = `${
   //   document.getElementById("resumeDetails").children[0].children[0].innerText
   // }_resume.pdf`;
-  const downloadPage = document.getElementById("cardImagesContainer");
-  var opt = {
-    margin: 0,
-    // filename: fileName,
-    image: { type: "jpeg", quality: 0.98 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-  };
-  html2pdf(downloadPage, opt);
+  // const downloadPage = document.getElementById("cardAsPdf");
+  kendo.drawing
+    .drawDOM($(document.getElementById("cardAsPdf")))
+    .then(function (group) {
+      kendo.drawing.pdf.saveAs(group, "file.pdf");
+    });
+  // var opt = {
+  //   margin: 1,
+  //   // filename: fileName,
+  //   image: { type: "JPG", quality: 0.98 },
+  //   html2canvas: { dpi: 192, letterRendering: true },
+  //   jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  // };
+  // html2pdf(downloadPage, opt);
 }
